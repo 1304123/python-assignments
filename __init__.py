88750fe11139777f4f63af72350343697a209f78
milamailo <miila.maiilo@gmail.com>
2024-03-17 18:39:29 -0400

    add __init__ to project

--- __init__.py
+++ __init__.py
@@ -1,19 +1,15 @@
 """
 Employee Management System Module
 
-This module provides various functionalities for managing employee data, 
-including adding, deleting, updating, and generating reports.
+This module provides various functionalities for managing employee data.
 """
+
+# Define the list of modules to be imported when the package is imported
+# This list specifies the modules that will be imported when using "from package import *"
+# In this case, only the main_menu module is exposed to the user
+__all__ = ['main_menu']
+
+# Import the main_menu module from the src.menu submodule
+# This import statement allows the main_menu function to be accessed directly from the package
+# Users can use "from package import main_menu" to access the main_menu function
 from .src.menu import main_menu
-
-def main():
-    """
-    Main function to start the Employee Management System.
-    
-    This function calls the main_menu() function to display the main menu
-    of the Employee Management System to the user.
-    """
-    main_menu()
-
-if __name__ == "__main__":
-    main()
