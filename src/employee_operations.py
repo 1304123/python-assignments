16bccd43979005a769744cd0698a3190b2dae964
milamailo <miila.maiilo@gmail.com>
2024-03-17 18:18:06 -0400

    add the employees_oprations

--- src/employee_operations.py
+++ src/employee_operations.py
@@ -1,8 +1,32 @@
+"""
+Employee Operations Module
+
+This module provides functions for adding, deleting, and updating employee information.
+"""
+
 def add_employee():
-    pass
+    """
+    Add Employee Function
+
+    This function prompts the user to input details for a new employee and adds the employee
+    to the system.
+
+    """
 
 def delete_employee():
-    pass
+    """
+    Delete Employee Function
+
+    This function prompts the user to input the ID of the employee to be deleted and
+    removes the employee from the system.
+
+    """
 
 def update_employee():
-    pass
+    """
+    Update Employee Function
+
+    This function prompts the user to input the ID of the employee to be updated and
+    allows the user to modify the employee's information such as name, department, or salary.
+
+    """
