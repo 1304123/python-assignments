6f856314d12daa2a2061ac99fa1a0031c226ff30
milamailo <miila.maiilo@gmail.com>
2024-03-17 18:16:35 -0400

    add description to main function

--- src/menu.py
+++ src/menu.py
@@ -7,9 +7,15 @@ adding, deleting, updating employee information, generating reports, and exiting
 the application.
 """
 from .employee_operations import add_employee, delete_employee, update_employee
-from report_generation import generate_reports
+from .report_generation import generate_reports
 
 def main_menu():
+    """
+    Main Menu Function
+    Displays the main menu for the Employee Management System and prompts the user for input.
+    Based on the user's choice, it calls the corresponding functions to perform actions such as
+    adding, deleting, or updating employee information, generating reports, or exiting the program.
+    """
     while True:
         print("\nEmployee Management System")
         print("1. Add Employee")
@@ -17,7 +23,7 @@ def main_menu():
         print("3. Update Employee")
         print("4. Generate Reports")
         print("5. Exit")
-        
+
         choice = input("Enter your choice: ")
 
         if choice == "1":
@@ -33,4 +39,3 @@ def main_menu():
             break
         else:
             print("Invalid choice. Please enter a valid option.")
-
