a22aca511f08809b3f73ac31997884287f4c9472
milamailo <miila.maiilo@gmail.com>
2024-03-17 18:04:32 -0400

    init added to handle the mudules in project

---
+++ src/employee_operations.py
@@ -0,0 +1,8 @@
+def add_employee():
+    pass
+
+def delete_employee():
+    pass
+
+def update_employee():
+    pass
---
+++ src/menu.py
@@ -0,0 +1,28 @@
+from .employee_operations import add_employee, delete_employee, update_employee
+from report_generation import generate_reports
+
+def main_menu():
+    while True:
+        print("\nEmployee Management System")
+        print("1. Add Employee")
+        print("2. Delete Employee")
+        print("3. Update Employee")
+        print("4. Generate Reports")
+        print("5. Exit")
+        
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            add_employee()
+        elif choice == "2":
+            delete_employee()
+        elif choice == "3":
+            update_employee()
+        elif choice == "4":
+            generate_reports()
+        elif choice == "5":
+            print("Exiting the program. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please enter a valid option.")
+
